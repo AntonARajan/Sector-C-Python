0a0b956017b51e1fc8d12e9f7bb490dc97e5ae39
AntonARajan <31612274+AntonARajan@users.noreply.github.com>
2017-09-28 14:42:52 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
-r=random.randint(15,45) # gives random num
+r=random.randint(15,45) # gives a random num
 print(r)
 if r<25:
-  print(r)
+  print(r) # prints  r less than  the number 25
   print(": is less than 25")
-elif r==27:
-	print("27 is a multiple of 3 and 9, both")
+elif r==27: 
+	print("27 is a multiple of 3 and 9, both") # prints multiples of 27
 elif r>=30:
-	print("is greater than 30")
+	print("is greater than 30") #prints numbers greater tan 30
 else:
-	print("your number is: ", r)
+	print("your number is: ", r) #prints entered number
