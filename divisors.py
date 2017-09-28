7fc681faebd97a2c5da796c7fc6314ded71afa68
AntonARajan <31612274+AntonARajan@users.noreply.github.com>
2017-09-28 14:49:17 +0530

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-d=(int(input("Enter a Number:")))
+d=(int(input("Enter a Number:"))) # function integer used with a statement
 b=1
-for b in range(1,d):
+for b in range(1,d):# adding a range for entered number
 	if(d%b==0):
-		print(b)
+		print(b)#prints if d is divisible by b
 	
