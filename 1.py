70e6035b17484ce278dd7be79b2b8e2b3a1ba1a0
AntonARajan <31612274+AntonARajan@users.noreply.github.com>
2017-09-28 14:38:21 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+#the constant integer
 a=55
+# constant + 19
 print(a+19)
+# constant -43
 print(a-43)
+# constant /6
 print(a/6)
+# constant %9
 print(a%9)
+# constant *4
 print(a*4)
